0f31ea1d9d536aede4aeb95fb7f115d5446a9965
Maria Stokłosa <maria.stoklosa@netguru.pl>
2014-12-05 12:42:59 +0100

    Schema.rb update

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141205112538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doors", force: true do |t|
+    t.boolean  "open"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
